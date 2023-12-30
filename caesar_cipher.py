50423fa08114a8a81d472d9d0a6128b58508635f
mapletoot <149334794+mapletoot@users.noreply.github.com>
2023-12-30 20:34:16 +0000

    encode and decode in same function

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -2,37 +2,51 @@
 Pseudo code:
 # Create a list called alphabet storing the alphabet.
 # This list will be used as a reference for the characters in the messages that we want to encode/decode.
-# Write a function that decodes a message.
-# The function will iterate through characters in a message to decode,
+# Write a function that encodes a message.
+# The function will iterate through characters in a message to encode,
 # it will find the index of each character in the message from the alphabet string,
 # move the index by however many places the caesar shift requires,
 # and then append the letter at the new index to a new decoded message string.
+# The decode function will work in the same way, but use the encode shift * -1 as the decode shift
 '''
 
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 # asking for inputs and converting string to lower case as the alphabet is in lower case
-message = input("What message would you like to encode? ").lower()
-encode_offset = int(input("What caesar shift value would you like to use? "))
+encode_or_decode = input("Are you encoding a message? Type y/n: ").lower()
+while encode_or_decode != "y" and encode_or_decode != "n":
+    encode_or_decode = input(
+        "Please enter 'y' if you are encoding a message and 'n' if you are decoding a message: ").lower()
+
+if encode_or_decode == "y":
+    message = input("What message do you want to encode? ").lower()
+    offset_value = int(input('''What caesar shift value would you like to use to encode your message? \n If you want A to become B please enter 1 or -25. \nIf you want B to become A please enter -1 or 25. \netc.
+    '''))
+else:
+    message = input("What message are you decoding? ").lower()
+    offset_value = -1 * int(input('''What caesar shift value was used to encode the message?
+    If A became B please enter 1 or -25.
+    If B became A please enter -1 or 25.
+    etc.
+    '''))
 
 
-def encode_caesar(coded_message, encode_shift):
+def caesar_shift(message_function, offset_value_function):
     ''' This function takes a message and shift value as an input and returns
     the decoded message
     '''
-    encoded_message = ""
-    for letter in coded_message:
+    shifted_message = ""
+    for letter in message_function:
         # This part changes all the letters in the string by the shift amount and adds to encoded message string
         if letter in alphabet:
             position = alphabet.find(letter)
-            new_character = alphabet[(position + encode_shift) % 26]
-            encoded_message += new_character
+            new_character = alphabet[(position + offset_value_function) % 26]
+            shifted_message += new_character
 
         # This part leaves the spaces and other characters alone and adds them to encoded message string
         else:
             new_character = letter
-            encoded_message += new_character
-    return encoded_message
-
-
-print(encode_caesar(message, encode_offset))
+            shifted_message += new_character
+    print(f"Your old message was {message_function}")
+    print(f"Your new message is {shifted_message}")
+    return shifted_message
